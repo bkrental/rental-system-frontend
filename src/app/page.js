50ea401c309827e3df729a09ef7fa2c39ce906a5
datdev2409 <datdev2409@gmail.com>
2024-01-24 08:35:49 +0700

    feat: test deploy

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-function LandingPage(){
-    return(
-        <div>
-            <h1>Landing Page</h1>
-        </div>
-    )
+function LandingPage() {
+  return (
+    <div>
+      <h1>Landing Page Deploy with Vercel</h1>
+    </div>
+  );
 }
 
 export default LandingPage;
