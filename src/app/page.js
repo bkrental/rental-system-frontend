4934af6f21bcba854a73a82c7042a8f36be32bdb
datdev2409 <datdev2409@gmail.com>
2024-02-08 15:06:44 +0700

    feat: test

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Button, Container, Page } from "@mui/material";
 import { BaseContainer } from "@components/BaseComponents";
 // import { StyledPage } from "@components/BaseComponents";
@@ -12,27 +12,26 @@ import styled from "@emotion/styled";
 // `
 
 const StyledPage = styled(BaseContainer)`
-    background: ${({ theme }) => theme.palette.background.page};
-    padding: 0;
-    margin: 0;
-`
-
+  background: ${({ theme }) => theme.palette.background.page};
+  padding: 0;
+  margin: 0;
+`;
 
 function LandingPage() {
-    // console.log((Link));
-    return (
-        <StyledPage>
-            {/* <div>Header</div> */}
-            <h1>Landing Page</h1>
+  // console.log((Link));
+  return (
+    <StyledPage>
+      {/* <div>Header</div> */}
+      <h1>Landing Page {process.env.TEXT}</h1>
 
-            <Button variant="contained" color="primary">
-                <Link href="/auth/login">Login</Link>
-            </Button>
-            <Button variant="contained" color="primary">
-                <Link href="/auth/signup">Signup</Link>
-            </Button>
-        </StyledPage>
-    )
+      <Button variant="contained" color="primary">
+        <Link href="/auth/login">Login</Link>
+      </Button>
+      <Button variant="contained" color="primary">
+        <Link href="/auth/signup">Signup</Link>
+      </Button>
+    </StyledPage>
+  );
 }
 
 export default LandingPage;
