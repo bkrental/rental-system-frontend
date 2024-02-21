87684036e42d50ccfb1b03f3d11c758b2c8ae765
Vide y Sora <nguyenphuoc2508.lhp.bk@gmail.com>
2024-02-21 09:44:29 +0700

    fix: add persist redux

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-    images: {
-        domains: ['datnguyen2409-bkrental-dev.s3.ap-southeast-1.amazonaws.com'],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'datnguyen2409-bkrental-dev.s3.ap-southeast-1.amazonaws.com'
+      }
+    ]
+  }
 }
