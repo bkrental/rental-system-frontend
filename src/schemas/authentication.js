b0a7e17ddbc16249d23e28b12807ec1e2c890abd
datdev2409 <datdev2409@gmail.com>
2024-03-17 19:46:23 +0700

    feat: add validation for phone number

--- src/schemas/authentication.js
+++ src/schemas/authentication.js
@@ -7,7 +7,12 @@ export const loginSchema = object({
 
 export const signUpSchema = object({
   name: string().required("Please enter your name"),
-  phone: string().required("Please enter your phone number"),
+  phone: string()
+    .matches(
+      /(\+84|84|0[3|5|7|8|9])+([0-9]{8})\b/g,
+      "Phone number must be a Vietnamese phone number.\n Ex: 0828696919 or +84828696919"
+    )
+    .required("Please enter your phone number"),
   email: string()
     .email("Invalid email format")
     .required("Please enter your email"),
