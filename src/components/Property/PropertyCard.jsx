8f25bc19c4af8b4e93171c198ec07aba19893ad8
datdev2409 <datdev2409@gmail.com>
2024-03-20 20:58:15 +0700

    refactor: property card css

--- src/components/Property/PropertyCard.jsx
+++ src/components/Property/PropertyCard.jsx
@@ -2,8 +2,11 @@ import BathtubIcon from "@mui/icons-material/Bathtub";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import SingleBedIcon from "@mui/icons-material/SingleBed";
 import "@scss/properties.scss";
+import "./PropertyCard.scss";
 import Image from "next/image";
 import Link from "next/link";
+import { FavoriteBorderRounded, HomeOutlined } from "@mui/icons-material";
+import HomeIcon from "@mui/icons-material/Home";
 
 export default function PropertyCard({
   property: {
@@ -23,61 +26,53 @@ export default function PropertyCard({
   };
 
   return (
-    <div className="property_card" key={_id}>
-      <div className="property_card-header">
+    <div className="propertyCard_container" key={_id}>
+      <div className="propertyCard_header">
         <Image
-          className="property_card-image"
+          className="propertyCard_thumbnail"
           src={thumbnail}
           fill
-          objectFit="cover"
           alt="property image"
+          sizes="100%"
         />
       </div>
 
-      <div className="property_card-body">
-        <h3 className="property_card-title">
+      <div className="propertyCard_body">
+        <h3 className="propertyCard_title">
           <Link href={`/rent/${_id}`}>{formatAddress(address)}</Link>
         </h3>
 
-        <div className="property_card-features">
-          <div className="property_card-features_item">
+        <div className="propertyCard_features">
+          <div className="propertyCard_features-item">
             <p>{`${area} m2`}</p>
           </div>
           {Boolean(bedrooms) && (
-            <div className="property_card-features_item">
+            <div className="propertyCard_features-item">
               <SingleBedIcon />
               <p>{`${bedrooms} phòng ngủ`}</p>
             </div>
           )}
           {Boolean(bathrooms) && (
-            <div className="property_card-features_item">
+            <div className="propertyCard_features-item">
               <BathtubIcon />
               <p>{`${1} phòng tắm`}</p>
             </div>
           )}
         </div>
 
-        <p className="property_card-price">{price + " triệu/tháng"}</p>
-
-        <p className="property_card-description">{description}</p>
+        <p className="propertyCard_price">{price + " triệu/tháng"}</p>
+        <p className="propertyCard_description">{description}</p>
       </div>
 
-      <div className="property_card-footer">
-        <div className="property_card-owner">
-          <div className="property_card-owner_avatar">CĐ</div>
-          <div className="property_card-owner_info">
-            <div className="property_card-owner_name">
-              {owner?.name || "Ẩn danh"}
-            </div>
-            <div className="property_card-published-time">Đăng hôm nay</div>
-          </div>
-        </div>
+      <div className="propertyCard_footer">
+        <div className="propertyCard_avatar">CĐ</div>
+        <div className="propertyCard_owner">{owner?.name || "Ẩn danh"}</div>
+        <div className="propertyCard_publishedDate">Đăng hôm nay</div>
 
-        <div className="property_card-favourite">
-          <FavoriteBorderIcon
-            sx={{ fontSize: 25, fontWeight: 400, color: "grey" }}
-          />
-        </div>
+        <FavoriteBorderRounded
+          className="propertyCard_favorite"
+          sx={{ fontSize: 25, fontWeight: 400, color: "grey" }}
+        />
       </div>
     </div>
   );
