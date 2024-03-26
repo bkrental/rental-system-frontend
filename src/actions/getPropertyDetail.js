265df1b3c01711eaa00a915381b44b612507f507
Dat Nguyen <98537938+datdev2409@users.noreply.github.com>
2024-03-26 08:00:58 +0700

    [Feature]: Add revalidate cache when fetching the property posts

--- src/actions/getPropertyDetail.js
+++ src/actions/getPropertyDetail.js
@@ -1,7 +1,9 @@
 "use server";
 export default async function getPropertyDetail(slug) {
   const baseURL = `${process.env.RENTAL_SERVICE_BACKEND_ENDPOINT}/posts/${slug}`;
-  const res = await fetch(baseURL);
+  const res = await fetch(baseURL, {
+    next: { revalidate: 20 },
+  });
 
   if (!res.ok) {
     throw new Error("Failed to fetch posts");
